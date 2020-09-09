8068bbee88589ccee3fb3ec99e0a8bb26b2a756e
RenanWeber <renanmweber@gmail.com>
2020-09-09 18:46:28 -0300

    Create main.c

---
+++ lista_01/e01/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main(void) {
+
+int varMetro = 0, varMM = 0 ;
+
+
+
+  printf("digite um valor em Metros\n");
+
+scanf("%d", &varMetro);
+
+varMM = varMetro * 1000;
+
+printf("valor em milimetros: %d \n",varMM);
+
+
+
+
+
+
+  return 0;
+}
